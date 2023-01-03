b2c5ae7a1cb71a60a2f0102ac47e69280c35a8bc
Jammal <oabass7@gmail.com>
2023-01-03 15:35:58 +0000

    mlflow

--- src/Adaboost.py
+++ src/Adaboost.py
@@ -15,8 +15,6 @@ mlflow.set_experiment("Project")
 
 df = pd.read_csv('./Data/Covid-19.csv')
 
-
-
 with mlflow.start_run(run_name="AdaBoost Classifier") as run:
 
     # Splitting the datasets and choosing 14 columns that are correlated with the DEATH Column
@@ -39,7 +37,7 @@ with mlflow.start_run(run_name="AdaBoost Classifier") as run:
     mlflow.log_param("scaler", "RobustScaler")
     mlflow.log_param("resampler", "RandomUnderSampler")
 
-    ada_boost = AdaBoostClassifier()
+    ada_boost = AdaBoostClassifier(learning_rate=0.5748335028625987, n_estimators=120,random_state=42)
     ada_boost.fit(X_train, y_train)
     y_preds = ada_boost.predict(X_test)
 
@@ -55,46 +53,20 @@ with mlflow.start_run(run_name="AdaBoost Classifier") as run:
     mlflow.log_metric("r2_score", r2_score(y_test,ada_boost.predict(X_test)))
     # mlflow.log_metric("classification_report", classification_report(y_test, y_preds))
 
-    plt.figure()
-    sns.heatmap(confusion_matrix(y_test, ada_boost.predict(X_test)), annot=True, fmt=".0f")
-    plt.title("AdaBoost Classifier Confusion Matrix",fontsize=18, color="b")
-    # plt.savefig("../Images/plots/adaboost_class_conf_matrix.jpeg")
-    mlflow.log_artifact("adaboost_class_conf_matrix.jpeg")
-    plt.show()
-
     # Log a classification report as an artifact
     with open("classification_report.txt", "w") as f:
         f.write(classification_report(y_test, y_preds))
     mlflow.log_artifact("classification_report.txt")
+
+    # Log a confusion matrix as an artifact
+    plt.figure()
+    sns.heatmap(confusion_matrix(y_test, ada_boost.predict(X_test)), annot=True, fmt=".0f")
+    plt.title("AdaBoost Classifier Confusion Matrix",fontsize=18, color="b")
+    plt.savefig("adaboost_conf_matrix.jpeg")
+    mlflow.log_artifact("adaboost_class_conf_matrix.jpeg")
 
     run_id = run.info.run_uuid
     experiment_id = run.info.experiment_id
     mlflow.end_run()
     print(f'artifact_uri = {mlflow.get_artifact_uri()}')
     print(f'runID: {run_id}')
-
-    
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-model = AdaBoostClassifier(learning_rate=0.5748335028625987, n_estimators=120,random_state=42)
-model.fit(X_train, y_train)
-print("Adaboost tune Accuracy: ",model.score(X_test,y_test))
